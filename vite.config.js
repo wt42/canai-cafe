56e887b4c649e7bb44592a6b0aa3f31cf045782e
Wijepala <thimira.wijepala@ssc-spc.gc.ca>
2026-06-25 15:26:26 -0700

    adjusted vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+// import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
+  base: "/canai-cafe/",
   server: {
     port: 5173,
-    open: true
-  }
+    open: true,
+  },
 });
